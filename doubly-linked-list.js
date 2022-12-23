545efa07d8cbd683d127feceaa075eddc644cab2
byunboy2 <76236947+byunboy2@users.noreply.github.com>
2022-12-22 16:13:15 -0800

    Finished

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -72,8 +72,8 @@ class DoublyLinkedList {
         this.tail = this.tail.prev;
         this.tail.next = null;
       } else {
-        this.tail = null;
         this.head = null;
+        this.tail = null;
       }
       removed.prev = null;
       this.length--;
@@ -84,38 +84,156 @@ class DoublyLinkedList {
   /** shift(): remove first item & return its value */
 
   shift() {
-
+    if (!this.head) {
+      return null;
+    } else {
+      let removed = this.head;
+      if (this.head.next) {
+        this.head = this.head.next;
+        this.head.prev = null;
+      } else {
+        this.head = null;
+        this.tail = null;
+      }
+      removed.next = null;
+      this.length--;
+      return removed.val;
+    }
   }
 
   /** getAt(idx): get val at idx.*/
 
   getAt(idx) {
-
+    if (idx < 0 || idx >= this.length) {
+      throw new Error("Index is out of range");
+    }
+    let current;
+    let midPoint = Math.floor(this.length / 2);
+    if (idx <= midPoint) {
+      current = this.head;
+      let start = 0;
+      while (idx > start) {
+        current = current.next;
+        start++;
+      }
+    }
+    if (idx >= midPoint) {
+      current = this.tail;
+      let start = this.length - 1;
+      while (idx < start) {
+        current = current.prev;
+        start--;
+      }
+    }
+    return current.val;
   }
+
 
   /** setAt(idx, val): set val at idx to val */
 
   setAt(idx, val) {
-
+    if (idx < 0 || idx >= this.length) {
+      throw new Error("Index is out of range");
+    }
+    let current;
+    let midPoint = Math.floor(this.length / 2);
+    if (idx <= midPoint) {
+      current = this.head;
+      let start = 0;
+      while (idx > start) {
+        current = current.next;
+        start++;
+      }
+    }
+    if (idx >= midPoint) {
+      current = this.tail;
+      let start = this.length - 1;
+      while (idx < start) {
+        current = current.prev;
+        start--;
+      }
+    }
+    current.val = val;
+    return current.val;
   }
 
   /** insertAt(idx, val): add node w/val before idx. */
 
   insertAt(idx, val) {
+    if (idx < 0 || idx > this.length) {
+      throw new Error("Index is out of range!");
+    }
 
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    if (idx === 0) {
+      // insert at the beginning of the list
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    } else if (idx === this.length) {
+      // insert at the end of the list
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
+    } else {
+      // insert in the middle of the list
+      let current = this.head;
+      for (let i = 0; i < idx - 1; i++) {
+        current = current.next;
+      }
+      newNode.next = current.next;
+      newNode.prev = current;
+      current.next.prev = newNode;
+      current.next = newNode;
+    }
+    this.length++;
   }
 
   /** removeAt(idx): return & remove item at idx, */
 
   removeAt(idx) {
+    if (idx < 0 || idx >= this.length) {
+      throw new Error("Index is out of range!");
+    }
 
+    if (this.length === 0) {
+      return null;
+    }
+
+    let removedVal;
+    if (idx === 0) {
+      // remove the head node
+      removedVal = this.head.val;
+      this.head = this.head.next;
+      if (this.head) {
+        this.head.prev = null;
+      } else {
+        this.tail = null;
+      }
+    } else if (idx === this.length - 1) {
+      // remove the tail node
+      removedVal = this.tail.val;
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+    } else {
+      // remove a node from the middle of the list
+      let current = this.head;
+      for (let i = 0; i < idx; i++) {
+        current = current.next;
+      }
+      removedVal = current.val;
+      current.prev.next = current.next;
+      current.next.prev = current.prev;
+    }
+
+    this.length--;
+    return removedVal;
   }
 
-  /** return average (mean) of list values. */
-
-  average() {
-
-  }
 }
 
 module.exports = DoublyLinkedList;
